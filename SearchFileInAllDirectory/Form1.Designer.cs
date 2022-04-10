f9056dbac8d4ff591d992b49f8f27ac8455f3708
“Nayeem-Bhuiyangit config --global user.email “nayeembhuiyan40@gmail.com <58185521+Nayeem-Bhuiyan@users.noreply.github.com>
2022-04-10 19:20:51 +0600

    sdfsdf

--- SearchFileInAllDirectory/Form1.Designer.cs
+++ SearchFileInAllDirectory/Form1.Designer.cs
@@ -104,6 +104,7 @@
             this.Controls.Add(this.list_SearchFileDisplay);
             this.Controls.Add(this.txtSearchKeyWord);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.grid_ShowFilePath)).EndInit();
