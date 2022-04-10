9899c66fea7cc2aa6c14e51db8d5cb605464506d
“Nayeem-Bhuiyangit config --global user.email “nayeembhuiyan40@gmail.com <58185521+Nayeem-Bhuiyan@users.noreply.github.com>
2022-04-10 17:34:07 +0600

    hgjhghg

--- SearchFileInAllDirectory/Form1.Designer.cs
+++ SearchFileInAllDirectory/Form1.Designer.cs
@@ -94,6 +94,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.grid_ShowFilePath);
             this.Controls.Add(this.btnBrowse);
