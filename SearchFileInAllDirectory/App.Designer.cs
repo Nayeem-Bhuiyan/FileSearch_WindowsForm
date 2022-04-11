336c50238d4b2ea0bde677dfba7fc36227ff5e5d
Nayeem-Bhuiyan <nayeembhuiyan40@gmail.com>
2022-04-11 10:37:28 +0600

    fghfgh

---
+++ SearchFileInAllDirectory/App.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace SearchFileInAllDirectory
+{
+    partial class App
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSearchKeyWord = new System.Windows.Forms.TextBox();
+            this.list_SearchFileDisplay = new System.Windows.Forms.ListBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtFolderDirectory = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.grid_ShowFilePath = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.grid_ShowFilePath)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtSearchKeyWord
+            // 
+            this.txtSearchKeyWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearchKeyWord.Location = new System.Drawing.Point(752, 22);
+            this.txtSearchKeyWord.Multiline = true;
+            this.txtSearchKeyWord.Name = "txtSearchKeyWord";
+            this.txtSearchKeyWord.Size = new System.Drawing.Size(185, 34);
+            this.txtSearchKeyWord.TabIndex = 0;
+           
+            // 
+            // list_SearchFileDisplay
+            // 
+            this.list_SearchFileDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.list_SearchFileDisplay.FormattingEnabled = true;
+            this.list_SearchFileDisplay.ItemHeight = 20;
+            this.list_SearchFileDisplay.Location = new System.Drawing.Point(47, 60);
+            this.list_SearchFileDisplay.Name = "list_SearchFileDisplay";
+            this.list_SearchFileDisplay.Size = new System.Drawing.Size(1011, 124);
+            this.list_SearchFileDisplay.TabIndex = 1;
+            this.list_SearchFileDisplay.SelectedIndexChanged += new System.EventHandler(this.list_SearchFileDisplay_SelectedIndexChanged);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(940, 22);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(117, 34);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtFolderDirectory
+            // 
+            this.txtFolderDirectory.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFolderDirectory.Location = new System.Drawing.Point(47, 22);
+            this.txtFolderDirectory.Multiline = true;
+            this.txtFolderDirectory.Name = "txtFolderDirectory";
+            this.txtFolderDirectory.Size = new System.Drawing.Size(466, 30);
+            this.txtFolderDirectory.TabIndex = 3;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(515, 20);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(117, 33);
+            this.btnBrowse.TabIndex = 4;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // grid_ShowFilePath
+            // 
+            this.grid_ShowFilePath.AllowUserToAddRows = false;
+            this.grid_ShowFilePath.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.grid_ShowFilePath.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.grid_ShowFilePath.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid_ShowFilePath.Location = new System.Drawing.Point(47, 201);
+            this.grid_ShowFilePath.Name = "grid_ShowFilePath";
+            this.grid_ShowFilePath.Size = new System.Drawing.Size(1011, 290);
+            this.grid_ShowFilePath.TabIndex = 5;
+            this.grid_ShowFilePath.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.grid_ShowFilePath_CellClick_1);
+            // 
+            // App
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1080, 503);
+            this.Controls.Add(this.grid_ShowFilePath);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtFolderDirectory);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.list_SearchFileDisplay);
+            this.Controls.Add(this.txtSearchKeyWord);
+            this.Name = "App";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "App";
+            this.Load += new System.EventHandler(this.App_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.grid_ShowFilePath)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSearchKeyWord;
+        private System.Windows.Forms.ListBox list_SearchFileDisplay;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtFolderDirectory;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.DataGridView grid_ShowFilePath;
+    }
+}
+
---
+++ SearchFileInAllDirectory/App.cs
@@ -0,0 +1,319 @@
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SearchFileInAllDirectory
+{
+    public partial class App : Form
+    {
+        public App()
+        {
+
+            InitializeComponent();
+
+         
+        }
+
+        //private IEnumerable<string> RecursiveFileSearch(string path, string pattern, ICollection<string> filePathCollector = null)
+        //{
+        //    try
+        //    {
+        //        filePathCollector = filePathCollector ?? new LinkedList<string>();
+
+        //        var matchingFilePaths = Directory.GetFiles(path, pattern);
+
+        //        foreach (var matchingFile in matchingFilePaths)
+        //        {
+        //            filePathCollector.Add(matchingFile);
+        //        }
+
+        //        var subDirectories = Directory.EnumerateDirectories(path);
+
+        //        foreach (var subDirectory in subDirectories)
+        //        {
+        //            RecursiveFileSearch(subDirectory, pattern, filePathCollector);
+        //        }
+
+        //        return filePathCollector;
+        //    }
+        //    catch (Exception error)
+        //    {
+        //        bool isIgnorableError = error is PathTooLongException ||
+        //            error is UnauthorizedAccessException;
+
+        //        if (isIgnorableError)
+        //        {
+        //            return Enumerable.Empty<string>();
+        //        }
+
+        //        throw error;
+        //    }
+        //}
+
+        private List<String> FileSearch()
+        {
+            List<String> files = new List<String>();
+            try
+            {
+            
+                string[] filesList = Directory.GetFiles(txtFolderDirectory.Text.ToString()!=""? txtFolderDirectory.Text.ToString(): @"C:\", "*.*", SearchOption.AllDirectories);
+                files.AddRange(filesList);
+
+            }
+            catch (System.Exception excpt)
+            {
+                MessageBox.Show(excpt.Message);
+            }
+
+            return files;
+            
+        }
+       
+
+     
+
+
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.WaitCursor;
+
+            Thread t = new Thread(new ThreadStart(Splash));
+            t.Start();
+            string str = string.Empty;
+            for (int i = 0; i < 1000000; i++)
+            {
+                str += i.ToString();
+            }
+
+            t.Abort();
+
+
+
+            btnSearch.Text="Loading";
+
+            grid_ShowFilePath.Columns.Clear();
+            DataTable dt = new DataTable();
+            dt.Columns.Add("File Name");
+
+
+            DataGridViewButtonColumn btnBrowseColumn = new DataGridViewButtonColumn();
+            btnBrowseColumn.UseColumnTextForButtonValue = true;
+            btnBrowseColumn.Text = "Browse";
+            btnBrowseColumn.Name = "btnBrowseFolder";
+            btnBrowseColumn.HeaderText = "Action";
+            grid_ShowFilePath.Columns.Add(btnBrowseColumn);
+
+
+            IEnumerable<string> filePathList = FileSearch();
+            if (filePathList!=null)
+            {
+                list_SearchFileDisplay.Items.Clear();
+
+                foreach (var path in filePathList)
+                {
+                    string fileName = Path.GetFileName(path);
+                    if (fileName.ToLower().StartsWith(txtSearchKeyWord.Text.Trim().ToLower().ToString()))
+                    {
+                        list_SearchFileDisplay.Items.Add(path);
+
+                        dt.Rows.Add(new object[] { path });
+
+                    }
+
+                }
+
+
+                grid_ShowFilePath.DataSource = dt;
+
+                btnSearch.Text="Search";
+                Cursor.Current = Cursors.Default;
+              
+                MessageBox.Show("Total "+list_SearchFileDisplay.Items.Count+" file found");
+            }
+            else
+            {
+                MessageBox.Show("No Match File Found");
+            }
+
+        }
+
+
+        private void Splash()
+        {
+            //Open a splash screen form
+            SplashScreen.SplashForm frm = new SplashScreen.SplashForm();
+            frm.Font = new Font("Time New Romans", 7);
+            frm.AppName = "Demo";
+            //frm.Icon = Properties.Resources.app;//Load icon from resource
+            frm.ShowIcon = true;
+            frm.ShowInTaskbar = true;
+            Application.Run(frm);
+        }
+
+
+
+
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
+            if (folderBrowser.ShowDialog() == DialogResult.OK)
+                txtFolderDirectory.Text=folderBrowser.SelectedPath;
+            else
+                MessageBox.Show("please Select folder");
+        }
+
+        private void list_SearchFileDisplay_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Clipboard.SetDataObject(this.list_SearchFileDisplay.SelectedItem.ToString());
+        }
+
+        private void grid_ShowFilePath_CellClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            grid_ShowFilePath.Rows[e.RowIndex].Selected = true;
+
+            //System.Diagnostics.Process.Start(grid_ShowFilePath.Rows[e.RowIndex].Cells[1].Value.ToString());
+
+            //CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            //dialog.InitialDirectory = Path.GetDirectoryName(grid_ShowFilePath.Rows[e.RowIndex].Cells[1].Value.ToString());
+            //dialog.IsFolderPicker = true;
+            //dialog.ShowHiddenItems=true;
+            //if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            //{
+            //    txtFolderDirectory.Text = dialog.FileName;
+            //}
+
+
+            OpenFileDialog openFolder = new OpenFileDialog
+            {
+                InitialDirectory = Path.GetDirectoryName(grid_ShowFilePath.Rows[e.RowIndex].Cells[1].Value.ToString()),
+                Title = "Browse Files",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                //DefaultExt = "txt",
+                //Filter = "txt files (*.txt)|*.txt",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+
+            if (openFolder.ShowDialog() == DialogResult.OK)
+            {
+                txtFolderDirectory.Text = openFolder.FileName;
+            }
+
+        }
+
+        private void App_Load_1(object sender, EventArgs e)
+        {
+            txtFolderDirectory.Text=@"Y:\";
+        }
+
+    //    private void txtSearchKeyWord_KeyUp(object sender, KeyEventArgs e)
+    //    {
+    //        btnSearch.Text="Loading";
+
+    //        grid_ShowFilePath.Columns.Clear();
+    //        DataTable dt = new DataTable();
+    //        dt.Columns.Add("File Name");
+
+
+    //        DataGridViewButtonColumn btnBrowseColumn = new DataGridViewButtonColumn();
+    //        btnBrowseColumn.UseColumnTextForButtonValue = true;
+    //        btnBrowseColumn.Text = "Browse";
+    //        btnBrowseColumn.Name = "btnBrowseFolder";
+    //        btnBrowseColumn.HeaderText = "Action";
+    //        grid_ShowFilePath.Columns.Add(btnBrowseColumn);
+
+
+    //        IEnumerable<string> filePathList = FileSearch();
+    //        if (filePathList!=null)
+    //        {
+    //            list_SearchFileDisplay.Items.Clear();
+
+    //            foreach (var path in filePathList)
+    //            {
+
+    //                string fileName = Path.GetFileName(path);
+    //                if (fileName.ToLower().StartsWith(txtSearchKeyWord.Text.Trim().ToLower().ToString()))
+    //                {
+    //                    list_SearchFileDisplay.Items.Add(path);
+
+    //                    dt.Rows.Add(new object[] { path });
+
+    //                }
+
+    //            }
+
+
+    //            grid_ShowFilePath.DataSource = dt;
+
+    //            btnSearch.Text="Search";
+              
+    //        }
+    //        else
+    //        {
+    //            MessageBox.Show(e.ToString());
+    //        }
+
+    //    }
+    }
+
+   public static class Helper
+    {
+        public static string FindCode(this string text, int codeLength=0)
+        {
+
+            string stopAt = String.Empty;
+            var listOfSpecialCharecter = new[] { "_", "-", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "=", @"\" };
+            bool isTrue = false;
+            foreach (var specialCharecter in listOfSpecialCharecter)
+            {
+                isTrue=specialCharecter.Any(text.Contains);
+                if (isTrue)
+                {
+                    stopAt=specialCharecter;
+                }
+
+            }
+
+            if (!String.IsNullOrWhiteSpace(text))
+            {
+                int charLocation = text.IndexOf(stopAt, StringComparison.Ordinal);
+                if (codeLength>0)
+                {
+                    if (charLocation > 0)
+                    {
+                        return text.Substring(0, codeLength);
+                    }
+                }
+                else
+                {
+                    if (charLocation > 0)
+                    {
+                        return text.Substring(0, charLocation);
+                    }
+                }
+
+            }
+
+            return String.Empty;
+        }
+    }
+
+}
