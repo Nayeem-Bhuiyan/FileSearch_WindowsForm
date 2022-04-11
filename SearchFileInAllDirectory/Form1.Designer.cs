c2f70a4e511588f65e8c1996c6e8d58874f40f94
Nayeem-Bhuiyan <nayeembhuiyan40@gmail.com>
2022-04-11 11:09:40 +0600

    hgjghjghj

--- SearchFileInAllDirectory/Form1.Designer.cs
+++ SearchFileInAllDirectory/Form1.Designer.cs
@@ -106,7 +106,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.BackColor = System.Drawing.Color.Cyan;
             this.ClientSize = new System.Drawing.Size(1080, 503);
             this.Controls.Add(this.grid_ShowFilePath);
             this.Controls.Add(this.btnBrowse);
